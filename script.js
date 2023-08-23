dc5cb0f950095bc66212f7c16af2272f87f3cec3
Suvansarkar <suvansarkar1@gmail.com>
2023-08-23 11:16:55 +0530

    Added darken functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 const grid = document.getElementById("grid");
 let gridSize = 16;
 let pixelSize = grid.clientHeight/gridSize;
 let color = "#ff0000";
 let flag = false;
 let random = false;
+let darken = false;
 // #808080
 
 function clear(){
@@ -12,6 +14,57 @@ function clear(){
         item.style.backgroundColor = "#FFFFFF";
     });
 }
+
+function LightenDarkenColor(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+    
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+function convertRgb(rgb) {
+    // This will choose the correct separator, if there is a "," in your value it will use a comma, otherwise, a separator will not be used.
+    var separator = rgb.indexOf(",") > -1 ? "," : " ";
+  
+  
+    // This will convert "rgb(r,g,b)" into [r,g,b] so we can use the "+" to convert them back to numbers before using toString 
+    rgb = rgb.substr(4).split(")")[0].split(separator);
+  
+    // Here we will convert the decimal values to hexadecimal using toString(16)
+    var r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+  
+    // The return value is a concatenation of "#" plus the rgb values which will give you your hex
+    return "#" + r + g + b;
+  }
 
 function updateColor(value){
     color = value;
@@ -28,7 +81,7 @@ function updateCanvas(value){
 function getColor(){
     if (random===false) {
         return color;
-    }else {
+    }else if (random===true) {
         let randcolor = Math.floor(Math.random()*16777215).toString(16);
         return "#" + randcolor;
     }
@@ -36,11 +89,23 @@ function getColor(){
 
 makeCanvas();
 
+document.getElementById("darken").addEventListener("click", () => {
+    if (darken===false) {
+        darken=true;
+        random=false;
+    }else {
+        darken=false;
+        random=false;
+    }
+});
+
 document.getElementById("random").addEventListener("click", () => {
     if (random===false) {
         random=true;
+        darken=false;
     }else {
         random=false;
+        darken=false;
     }
 });
 
@@ -55,13 +120,18 @@ function makeCanvas(){
         div.classList.add("pixel");
         div.setAttribute("draggable", false);
         div.addEventListener("mousedown", () => {
-            div.style.backgroundColor = getColor();
+            if (darken===false) {
+                div.style.backgroundColor = getColor();
+            }else {
+                div.style.backgroundColor = LightenDarkenColor(convertRgb(div.style.backgroundColor), -10);
+                console.log(darken);
+            }
         });
         grid.appendChild(div);
     }    
     document.querySelectorAll(".pixel").forEach((item)=>{
         item.addEventListener("mouseover", ()=>{
-            if(flag===true){
+            if(flag===true && darken===false){
                 item.style.backgroundColor = getColor();
             }
         });
