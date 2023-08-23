d97975bd0824eb505cdc51c9c6c43ef12e88f7cc
Suvansarkar <suvansarkar1@gmail.com>
2023-08-23 09:36:55 +0530

    did stuff

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 const grid = document.getElementById("grid");
-const gridSize = 16;
-const pixelSize = grid.clientHeight/gridSize;
-let color = "#808080";
+let gridSize = 16;
+let pixelSize = grid.clientHeight/gridSize;
+let color = "#ff0000";
 let flag = false;
 
 // #808080
 
 function clear(){
     const div = document.querySelectorAll(".pixel");
-    // console.log(div);
     div.forEach((item)=> {
         item.style.backgroundColor = "#FFFFFF";
     });
 }
 
+function updateColor(value){
+    color = value;
+}
 
+function updateCanvas(value){
+    gridSize = value;
+    pixelSize = grid.clientHeight/gridSize;
+    console.log(value);
+    makeCanvas();
+}
+
+makeCanvas();
 
 document.getElementById("clear").addEventListener("click", clear);
 
-for (let i = 0; i < gridSize*gridSize; i++) {
-    console.log(pixelSize);
-    const div = document.createElement("div");
-    div.style.width = pixelSize + "px";
-    div.style.height = pixelSize + "px";
-    div.classList.add("pixel");
-    div.setAttribute("draggable", false);
-    div.addEventListener("mousedown", () => {
-        div.style.backgroundColor = color;
+function makeCanvas(){
+    document.querySelectorAll(".pixel").forEach((item)=>{item.remove()});
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const div = document.createElement("div");
+        div.style.width = pixelSize + "px";
+        div.style.height = pixelSize + "px";
+        div.classList.add("pixel");
+        div.setAttribute("draggable", false);
+        div.addEventListener("mousedown", () => {
+            div.style.backgroundColor = color;
+        });
+        grid.appendChild(div);
+    }    
+    document.querySelectorAll(".pixel").forEach((item)=>{
+        item.addEventListener("mouseover", ()=>{
+            if(flag===true){
+                item.style.backgroundColor = color;
+            }
+        });
     });
-    grid.appendChild(div);
 }
 
 document.addEventListener("mousedown", ()=>{
@@ -39,13 +58,6 @@ document.addEventListener("mouseup", ()=>{
     flag=false;
 });
 
-document.querySelectorAll(".pixel").forEach((item)=>{
-    item.addEventListener("mouseover", ()=>{
-        if(flag===true){
-            item.style.backgroundColor = color;
-        }
-    });
-});
 
 // document.addEventListener("mouseup", ()=>{
 //     document.querySelectorAll(".pixel").forEach((item)=>{
